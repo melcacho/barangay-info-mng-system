18558e0163279766b0bad92803e0f6edfd3b0bd0
afosana <fosana.aidel@ue.edu.ph>
2021-12-01 21:32:45 +0800

    Update resident-information.php

--- resident-information.php
+++ resident-information.php
@@ -105,24 +105,101 @@
             </nav>
 
             <div class="content">
-                <h2>Collapsible Sidebar Using Bootstrap 4</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="container-xl">
+                    <div class="mt-5 mb-3 clearfix d-flex">
+                        <h2 class="my-auto">Resident Information Management</h2>
 
-                <div class="line"></div>
+                        <a class="btn btn-success ml-auto mr-0 my-auto" 
+                        data-toggle="tooltip"
+                        href="?id=">
+                            <i class="fa fa-plus"></i> 
+                            New Resident
+                        </a>
+                    </div>
 
-                <h2>Lorem Ipsum Dolor</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div class="overflow-auto">
+                        <?php
+                            $myfile = fopen("assets/barangay-config/committee.txt", "r") or die("Unable to open file!");
+                            $a_committee = [];
+                            while(!feof($myfile)) {
+                                array_push($a_committee, fgets($myfile));
+                            }
+                            fclose($myfile);
 
-                <div class="line"></div>
+                            $myfile = fopen("assets/barangay-config/position.txt", "r") or die("Unable to open file!");
+                            $a_position = [];
+                            while(!feof($myfile)) {
+                                array_push($a_position, fgets($myfile));
+                            }
+                            fclose($myfile);
 
-                <h2>Lorem Ipsum Dolor</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            // Include config file
+                            require_once "config.php";
 
-                <div class="line"></div>
-
-                <h3>Lorem Ipsum Dolor</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            // Attempt select query execution
+                            $sql = "SELECT * FROM accounts";
+                            if ($result = $mysqli->query($sql)) {
+                                if ($result->num_rows > 0) {
+                                    echo '<table class="table table-bordered table-striped text-center">';
+                                    echo "<thead>";
+                                    echo "<tr class=\"bg-dark\">";
+                                    echo "<th>Family Name</th>";
+                                    echo "<th>First Name</th>";
+                                    echo "<th>Middle Name</th>";
+                                    echo "<th>Alias</th>";
+                                    echo "<th>Face Marks</th>";
+                                    echo "<th>Birth Date</th>";
+                                    echo "<th>Birth Place</th>";
+                                    echo "<th>Sex</th>";
+                                    echo "<th>Civil Status</th>";
+                                    echo "<th>Nationality</th>";
+                                    echo "<th>Religion / Belief</th>";
+                                    echo "<th>Occupation</th>";
+                                    echo "<th>Spouse's Name</th>";
+                                    echo "<th>Spouse's Occupation</th>";
+                                    echo "<th>Voter Status</th>";
+                                    echo "<th>Action</th>";
+                                    echo "</tr>";
+                                    echo "</thead>";
+                                    echo "<tbody>";
+                                    while ($row = $result->fetch_array()) {
+                                        echo "<tr>";
+                                        echo "<td>" . $row['LNAME'] . "</td>";
+                                        echo "<td>" . $row['FNAME'] . "</td>";
+                                        echo "<td>" . $row['MNAME'] . "</td>";
+                                        echo "<td>" . $row['ALIAS'] . "</td>";
+                                        echo "<td>" . $row['FACE_MARKS'] . "</td>";
+                                        echo "<td>" . $row['BIRTH_DATE'] . "</td>";
+                                        echo "<td>" . $row['BIRTH_PLACE'] . "</td>";
+                                        echo "<td>" . $row['SEX'] . "</td>";
+                                        echo "<td>" . $row['CIVIL_STATUS'] . "</td>";
+                                        echo "<td>" . $row['NATIONALITY'] . "</td>";
+                                        echo "<td>" . $row['RELIGION_BELIEF'] . "</td>";
+                                        echo "<td>" . $row['OCCUPATION'] . "</td>";
+                                        echo "<td>" . $row['SPOUSE_NAME'] . "</td>";
+                                        echo "<td>" . $row['SPOUSE_OCCUPATION'] . "</td>";
+                                        echo "<td>" . $row['VOTER_STATUS'] . "</td>";
+                                        echo "<td>";
+                                        echo '<a href="?id=' . $row['ID'] . '" class="mr-3 action" title="Update Record" data-toggle="tooltip"><span class="fas fa-pencil-alt"></span></a>';
+                                        echo '<a href="?id=' . $row['ID'] . '&delete=0" class="action" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                        echo "</td>";
+                                        echo "</tr>";
+                                    }
+                                    echo "</tbody>";
+                                    echo "</table>";
+                                    // Free result set
+                                    $result->free();
+                                } else {
+                                    echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                                }
+                            } else {
+                                echo '<script>
+                                alert("Oops! Something went wrong. Please try again later.");
+                                </script>';
+                            }
+                        ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
